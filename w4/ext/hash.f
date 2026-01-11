b93a51ac5165d4f49002ccef7744bdb4470a3744
Jaco <jacogr@gmail.com>
2026-01-11 19:48:53 +0200

    Add fmix32, align hash with host (untested)

--- w4/ext/hash.f
+++ w4/ext/hash.f
@@ -1,21 +1,22 @@
 require ../std/loops.f
 
 \ djb2a hash
+\ loops: ((hash << 5) + hash) ^ ch
 
-	: djb2a ( c-addr u -- u ) \ ((hash << 5) + hash) ^ ch
+	: djb2a ( c-addr u -- u )
 		$1505 swap 0		( c-addr u -- c-addr hash u 0 -- )
 		do					( c-addr hash u 0 -- c-addr hash )
 			over i + c@		( c-addr hash --  c-addr hash ch )
 			swap			( c-addr hash ch -- c-addr ch hash )
-			#33 *			\ equivalent to (hash * 33) ^ ch
-			xor
+			5 lshift xor	\ equivalent to (hash << 5) ^ ch
 		loop
 		nip					( c-addr hash -- hash )
 	;
 
 \ fnv1a hash
+\ loops: (hash ^ ch) * prime
 
-	: fnv1a ( c-addr u -- u ) \ (hash ^ ch) * prime
+	: fnv1a ( c-addr u -- u )
 		$811c9dc5 swap 0	( c-addr u -- c-addr hash u 0 -- )
 		do					( c-addr hash u 0 -- c-addr hash )
 			over i + c@		( c-addr hash --  c-addr hash ch )
@@ -24,13 +25,19 @@ require ../std/loops.f
 		nip					( c-addr hash -- hash )
 	;
 
+\ fmix32
+
+	: fmix32 ( u -- u' )
+		dup 16 rshift xor	\ h ^= h >> 16
+		0x85ebca6b *		\ h *= 0x85ebca6b
+		dup 13 rshift xor	\ h ^= h >> 13
+		0xc2b2ae35 *		\ h *= 0xc2b2ae35
+		dup 16 rshift xor	\ h ^= h >> 16
+	;
+
 \ host-compatible hash values for lookups
 
-	: host::hash ( c-addr u -- hd hf )
-		2dup		( c-addr u -- c-addr u c-addr u )
-		fnv1a		( ... c-addr u -- c-addr u hf )
-	 	>r djb2a r>	( c-addr u -- hd hf )
-	;
+	: host::hash ( c-addr u -- u ) fnv1a fmix32	;
 
 	: host::hash'
 		parse-name host::hash
