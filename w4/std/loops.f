b73c1c6dadecceb85fa6ae67923e78a9d7dafa02
Jaco <jacogr@gmail.com>
2026-01-16 20:33:04 +0200

    Add roll word

--- w4/std/loops.f
+++ w4/std/loops.f
@@ -334,5 +334,24 @@ require stack.f
 	\ : ?exit postpone if postpone exit postpone then ; immediate
 
 \ https://forth-standard.org/standard/core/qDUP
+\
+\ Duplicate x if it is non-zero.
 
 	: ?dup dup if dup then ;
+
+\ https://forth-standard.org/standard/core/ROLL
+\
+\ Remove u. Rotate u+1 items on the top of the stack. An ambiguous condition
+\ exists if there are less than u+2 items on the stack before ROLL is executed.
+
+	: roll ( x0 i*x u.i -- i*x x0 )
+		dup 0= if
+			drop
+			exit
+		then
+
+		swap >r
+		1- recurse
+		r> swap
+	;
+
