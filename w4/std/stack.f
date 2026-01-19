cc93624ab78d102452ab98ba695efaf9f5a766f9
Jaco <jacogr@gmail.com>
2026-01-19 07:27:34 +0200

    Add 2rot

--- w4/std/stack.f
+++ w4/std/stack.f
@@ -190,3 +190,15 @@ require stack.ptr.f
 	: 2r@ ( r: x y ) ( -- x y )
 		r-2@ r-1@
 	;
+
+\ https://forth-standard.org/standard/core-ext/TwoROT
+\
+\ Rotate the top three cell pairs on the stack bringing cell pair
+\ x1 x2 to the top of the stack.
+
+	: 2rot ( x1 x2 x3 x4 x5 x6 -- x3 x4 x5 x6 x1 x2 )
+		>r >r	\ x1 x2 x3 x4       ( r: -- x6 x5 )
+		2swap	\ x3 x4 x1 x2
+		r> r>	\ x3 x4 x1 x2 x5 x6 ( r: x6 x5 -- )
+		2swap	\ x3 x4 x5 x6 x1 x2
+	;
