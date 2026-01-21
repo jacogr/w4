6c29a2cd83193675e6cd50c9afef8e013da9e6fd
Jaco <jacogr@gmail.com>
2026-01-21 14:17:02 +0200

    Rename string.c

--- w4/w4.f
+++ w4/w4.f
@@ -75,7 +75,7 @@ require std/stack.loop.f
 require std/stack.ptr.f
 require std/stdio.f
 require std/string.f
-require std/string.c.f
+require std/string.utils.f
 require std/test.f
 require std/text.f
 require std/unimplemented.f
