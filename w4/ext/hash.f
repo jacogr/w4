8d7cc112ef43b682339c3b48084570ceca08e30b
Jaco <jacogr@gmail.com>
2026-01-27 15:06:46 +0200

    Adjust to correct imports in hash

--- w4/ext/hash.f
+++ w4/ext/hash.f
@@ -1,4 +1,6 @@
 require ../std/loops.f
+require ../std/stack.loop.f
+require ../std/string.utils.f
 
 \ djb2a hash
 \ loops: ((hash << 5) + hash) ^ ch
@@ -6,15 +8,15 @@ require ../std/loops.f
 \ https://en.wikipedia.org/wiki/Daniel_J._Bernstein
 
 	: DJB2A-i ( c-addr u -- u )
-		$1505 swap			( c-addr u -- c-addr hash u -- )
-		0 ?do				( c-addr hash u 0 -- c-addr hash )
-			over i +		( c-addr hash -- c-addr hash ch-addr )
-			c@ >lower-ascii ( c-addr hash ch-addr -- c-addr hash ch )
-			swap dup		( c-addr hash ch -- c-addr ch hash hash )
-			5 lshift		( c-addr ch hash hash -- c-addr ch hash hash<<5 )
-			+ xor			( c-addr ch hash hash<<5 -- c-addr hash )	\ ((hash << 5) + hash) ^ ch
+		$1505 swap				( c-addr u -- c-addr hash u -- )
+		0 ?do					( c-addr hash u 0 -- c-addr hash )
+			over i +			( c-addr hash -- c-addr hash ch-addr )
+			c@ >lower-ascii		( c-addr hash ch-addr -- c-addr hash ch )
+			swap dup			( c-addr hash ch -- c-addr ch hash hash )
+			5 lshift			( c-addr ch hash hash -- c-addr ch hash hash<<5 )
+			+ xor				( c-addr ch hash hash<<5 -- c-addr hash )	\ ((hash << 5) + hash) ^ ch
 		loop
-		nip					( c-addr hash -- hash )
+		nip						( c-addr hash -- hash )
 	;
 
 \ fnv1a hash
@@ -23,13 +25,13 @@ require ../std/loops.f
 \ https://en.wikipedia.org/wiki/Fowler%E2%80%93Noll%E2%80%93Vo_hash_function
 
 	: FNV1A-i ( c-addr u -- u )
-		$811c9dc5 swap		( c-addr u -- c-addr hash u )
-		0 ?do				( c-addr hash u 0 -- c-addr hash )
-			over i +		( c-addr hash -- c-addr hash ch-addr )
-			c@ >lower-ascii	( c-addr hash ch-addr -- c-addr hash ch )
-			xor	$01000193 *	( c-addr hash ch -- c-addr hash' )
+		$811c9dc5 swap			( c-addr u -- c-addr hash u )
+		0 ?do					( c-addr hash u 0 -- c-addr hash )
+			over i +			( c-addr hash -- c-addr hash ch-addr )
+			c@ >lower-ascii		( c-addr hash ch-addr -- c-addr hash ch )
+			xor	$01000193 *		( c-addr hash ch -- c-addr hash' )
 		loop
-		nip					( c-addr hash -- hash )
+		nip						( c-addr hash -- hash )
 	;
 
 \ fmix32
@@ -37,11 +39,11 @@ require ../std/loops.f
 \ https://github.com/aappleby/smhasher/blob/master/src/MurmurHash3.cpp
 
 	: FMIX32 ( u -- u' )
-		dup 16 rshift xor	\ h ^= h >> 16
-		$85ebca6b *			\ h *= 0x85ebca6b
-		dup 13 rshift xor	\ h ^= h >> 13
-		$c2b2ae35 *			\ h *= 0xc2b2ae35
-		dup 16 rshift xor	\ h ^= h >> 16
+		dup 16 rshift xor		\ h ^= h >> 16
+		$85ebca6b *				\ h *= 0x85ebca6b
+		dup 13 rshift xor		\ h ^= h >> 13
+		$c2b2ae35 *				\ h *= 0xc2b2ae35
+		dup 16 rshift xor		\ h ^= h >> 16
 	;
 
 \ host-compatible hash values for lookups, applies fmix32(fnv1a(value)), if
